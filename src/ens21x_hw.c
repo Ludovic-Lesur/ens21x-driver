93a7ae60cd317ef2c3699aaf148cd8ef912ed5c3
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-03-15 11:48:02 +0100

    Return error by default in HW weak functions.

--- src/ens21x_hw.c
+++ src/ens21x_hw.c
@@ -20,7 +20,7 @@
 /*******************************************************************/
 ENS21X_status_t __attribute__((weak)) ENS21X_HW_init(void) {
     // Local variables.
-    ENS21X_status_t status = ENS21X_SUCCESS;
+    ENS21X_status_t status = ENS21X_ERROR_HW_FUNCTION_NOT_IMPLEMENTED;
     /* To be implemented */
     return status;
 }
@@ -28,7 +28,7 @@ ENS21X_status_t __attribute__((weak)) ENS21X_HW_init(void) {
 /*******************************************************************/
 ENS21X_status_t __attribute__((weak)) ENS21X_HW_de_init(void) {
     // Local variables.
-    ENS21X_status_t status = ENS21X_SUCCESS;
+    ENS21X_status_t status = ENS21X_ERROR_HW_FUNCTION_NOT_IMPLEMENTED;
     /* To be implemented */
     return status;
 }
@@ -36,7 +36,7 @@ ENS21X_status_t __attribute__((weak)) ENS21X_HW_de_init(void) {
 /*******************************************************************/
 ENS21X_status_t __attribute__((weak)) ENS21X_HW_i2c_write(uint8_t i2c_address, uint8_t* data, uint8_t data_size_bytes, uint8_t stop_flag) {
     // Local variables.
-    ENS21X_status_t status = ENS21X_SUCCESS;
+    ENS21X_status_t status = ENS21X_ERROR_HW_FUNCTION_NOT_IMPLEMENTED;
     /* To be implemented */
     UNUSED(i2c_address);
     UNUSED(data);
@@ -48,7 +48,7 @@ ENS21X_status_t __attribute__((weak)) ENS21X_HW_i2c_write(uint8_t i2c_address, u
 /*******************************************************************/
 ENS21X_status_t __attribute__((weak)) ENS21X_HW_i2c_read(uint8_t i2c_address, uint8_t* data, uint8_t data_size_bytes) {
     // Local variables.
-    ENS21X_status_t status = ENS21X_SUCCESS;
+    ENS21X_status_t status = ENS21X_ERROR_HW_FUNCTION_NOT_IMPLEMENTED;
     /* To be implemented */
     UNUSED(i2c_address);
     UNUSED(data);
@@ -59,7 +59,7 @@ ENS21X_status_t __attribute__((weak)) ENS21X_HW_i2c_read(uint8_t i2c_address, ui
 /*******************************************************************/
 ENS21X_status_t __attribute__((weak)) ENS21X_HW_delay_milliseconds(uint32_t delay_ms) {
     // Local variables.
-    ENS21X_status_t status = ENS21X_SUCCESS;
+    ENS21X_status_t status = ENS21X_ERROR_HW_FUNCTION_NOT_IMPLEMENTED;
     /* To be implemented */
     UNUSED(delay_ms);
     return status;
